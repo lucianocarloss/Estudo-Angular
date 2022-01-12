fec435b93bddd183a5000cdfdeef42dd9ac3159b
Luciano <lucianos2vania@gmail.com>
2022-01-12 16:17:17 -0300

    Aula de Angular Inicio

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'Meu-Primeiro-Component',
+    template: `
+    <h2>Luciano</h2>
+    <p>Meu Primeiro Componente em Angular 2</p>
+    `
+    // template: '<p>Meu Primeiro Componente em Angular 2</p>'
+
+})
+export class MeuPrimeiroComponent { }
