3924e9bf3e7b1bd375814935190350b7dd2f710c
Luciano <lucianos2vania@gmail.com>
2022-01-26 15:40:36 -0300

    Service in Service

--- 05-servicos/src/app/app.module.ts
+++ 05-servicos/src/app/app.module.ts
@@ -2,23 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CursosComponent } from './cursos/cursos.component';
-import { CursosService } from './cursos/cursos.service';
-import { CriarCursoComponent } from './criar-curso/criar-curso.component';
+import { cursosModule } from './cursos/cursos.module';
+// import { CursosService } from './cursos/cursos.service';
+import { criarCursoModule } from './criar-curso/criar-curso.module';
+import { LogService } from './shared/log.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CursosComponent,
-    CriarCursoComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
+    criarCursoModule,
+    cursosModule
   ],
-  providers: [
-    CursosService
-  ],
+  providers: [LogService],
+  // providers: [
+  //   CursosService
+  // ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- 05-servicos/src/app/cursos/cursos.component.ts
+++ 05-servicos/src/app/cursos/cursos.component.ts
@@ -5,7 +5,8 @@ import { CursosService } from './cursos.service';
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
-  styleUrls: ['./cursos.component.css']
+  styleUrls: ['./cursos.component.css'],
+  providers: [ CursosService ]
 })
 export class CursosComponent implements OnInit {
 
@@ -21,6 +22,13 @@ export class CursosComponent implements OnInit {
   ngOnInit() {
 
     this.cursos = this.cursosService.getCursos();
+    // this.cursosService.emitirCursoCriado.subscribe(
+    //   curso => alert(curso)
+    // );
+
+    CursosService.criouNovoCurso.subscribe(
+      curso => this.cursos.push(curso)
+    );
 
   }
 
